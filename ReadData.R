04fe422bbc116b44f74b683de60b9f93e1f3eb1f
Andrew Resnikoff <andrewresnikoff@gmail.com>
2018-03-28 15:58:50 -0400

    wrapped ReadData.R into a function read_fitbit_data(path, pattern) so that it works with any directory structure

--- ReadData.R
+++ ReadData.R
@@ -1,12 +1,51 @@
-require(readxl)
-
-myFiles <- list.files(path = "../FitBit Data", pattern="*.xls")
-
-dat <- list()
-
-for(i in 1:length(myFiles)){
-  current <- paste("../FitBit Data/", myFiles[i], sep = "")
-  dat[[i]] <- read_excel(current, sheet = 2)
+clean_fitbit_column <- function(column){
+    
+    # remove commas
+    clean_column <- gsub(",", "", column)
+    
+    # try to parse column as numeric
+    try_parse <- as.numeric(clean_column)
+    
+    # if no rows can be parsed as numeric, we have a date column
+    if (all(is.na(as.numeric(try_parse)))){
+        # date column
+        return(strptime(column, "%Y-%m-%d"))
+    }
+    # otherwise treat the column as numeric
+    else{
+        return(try_parse)
+    }
+    
 }
 
-timeData <- as.data.frame(do.call(rbind, dat))
+read_fitbit_data <- function(path, pattern = "*.xls"){
+    
+    # load required package
+    if (!require('readxl')){
+        install.packages("readxl")
+        library('readxl')
+    }
+    
+    # get a list of all files at 'path' matching the extension 'pattern'
+    myFiles <- list.files(path = path, pattern = pattern)
+    
+    # create a list that stores individual datasets for each file
+    dat = list()
+    for (i in 1:length(myFiles)){
+        current <- paste(path, "/", myFiles[i], sep = "")
+        dat[[i]] <- read_excel(current, sheet = 2)
+    }
+    
+    # merge the list into a single data frame
+    df <- as.data.frame(do.call(rbind, dat))
+    
+    # rename columns without spaces or periods
+    colnames(df) <- gsub(" ", "", colnames(df))
+    
+    # clean data frame into correct data types
+    df <- as.data.frame(suppressWarnings(sapply(df, clean_fitbit_column)))
+
+    return(df)
+    
+} 
+
